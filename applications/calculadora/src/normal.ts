3fdce63b8d2b87948bc610a819cd28af3b637b9c
Alvaro Muñoz Garcia <amgarcia@arsys.es>
2025-12-09 12:49:25 +0100

    feat(ejercicio3): financiera

---
+++ applications/calculadora/src/normal.ts
@@ -0,0 +1,19 @@
+const sumar: ((a: number, b: number) => number) = (a: number, b: number): number => {
+
+    const resultado = a + b;
+
+    console.info(`sumar ${a} y ${b} da ${resultado}`);
+
+    return resultado;
+};
+
+const restar: ((a: number, b: number) => number) = (a: number, b: number): number => {
+
+    const resultado = a - b;
+
+    console.info(`restar ${a} y ${b} da ${resultado}`);
+
+    return resultado;
+};
+
+export { sumar, restar };
