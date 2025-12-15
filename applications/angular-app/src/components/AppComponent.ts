ff6e876e8510717efffc45c7dc33452f629f4593
Alvaro Muñoz Garcia <amgarcia@arsys.es>
2025-12-15 11:22:45 +0100

    wip: angular micro service

---
+++ applications/angular-app/src/components/AppComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-root",
+    standalone: true,
+    template: `
+    Hola, soy una app de angular
+  `,
+})
+export class AppComponent { }
---
+++ applications/angular-app/src/index.ts
@@ -0,0 +1,11 @@
+import { inicio } from "./ui";
+
+const name: string = "angular-app";
+
+setTimeout(() => {
+
+    inicio("angular-app");
+
+}, 5000);
+
+export { name };
